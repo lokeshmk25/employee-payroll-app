f14db9f0e2b434237b25fdd06ff46c9253c11aa5
lokeshmk25 <lokeshmk2211@gmail.com>
2021-12-14 11:35:52 +0530

    Added: Controller advice to handle global and custom exceptions

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/CustomException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+public class CustomException extends RuntimeException{
+    public CustomException(String message) {
+        super(message);
+    }
+}
